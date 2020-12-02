3c7118728a78d77c5b6f63d6f92a869eb04f3a25
Mark Pham <markpham@Marks-iMac.local>
2020-12-01 19:14:47 -0500

    first

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Mark Pham on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Mark Pham on 12/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var total : Double = 0
+    let button_names = [
+        ["AC", "+/-", "%", "/"],
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", "="]
+    ]
+    
+    var body: some View {
+        
+        ZStack (alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 12) {
+                
+                HStack {
+                    Text(total.removeZerosFromEnd())
+                      .font(.system(size: 80))
+                      .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .bottomTrailing)
+                      .padding(.trailing, 30)
+                      .foregroundColor(.white)
+                }.padding()
+                
+                
+                ForEach(button_names, id: \.self) { row in
+                    HStack (spacing: 9) {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                            .font(.system(size: 32))
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                                .foregroundColor(.white)
+                                .background(Color.gray)
+                                .cornerRadius(self.buttonWidth())
+                        }
+                    }
+                }
+            }.padding(.bottom)
+        }
+    }
+    
+    func buttonWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
+    }
+}
+extension Double {
+    func removeZerosFromEnd() -> String {
+        let formatter = NumberFormatter()
+        let number = NSNumber(value: self)
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 16 //maximum digits in Double after dot (maximum precision)
+        return String(formatter.string(from: number) ?? "")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
